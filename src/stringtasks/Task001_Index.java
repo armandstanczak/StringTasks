743a65c7e5218bc0a4d93a4ff2cc27b45d02a29e
Armand Stanczak <armandstanczak@gmail.com>
2020-04-06 21:10:55 +0200

    String Tasks

---
+++ src/stringtasks/Task001_Index.java
@@ -0,0 +1,16 @@
+package stringtasks;
+
+/**
+ *
+ * @author Armand Stańczak
+ */
+public class Task001_Index {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
